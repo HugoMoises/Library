20bbec16119cb37efeeb7d385257bb6cef0f6fcb
Hugo Moisés <hugo2018moises@gmail.com>
2025-09-25 15:19:06 -0300

    Iniciando CRUD para livros utilizando Class Based Views; Página index e listagem de livros adicionadas; Configurações de urls para que as rotas funcionem

--- library/urls.py
+++ library/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import IndexView, LivroListView 
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('livros/', LivroListView.as_view(), name='livros_list'),
+
+]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Livro
+from .forms import LivroForm
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'livros/index.html'
+
+class LivroListView(ListView):
+    template_name = 'livros/livros_list.html'
+    model = Livro
+    context_object_name = 'livros'
+
+
 
     
